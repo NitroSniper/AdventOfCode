d48a106c629225f780153eba63092cb32d121c66
NitroSniper <mahiemiah35@gmail.com>
2024-12-03 02:26:58 +0000

    Finished Day 1 Python code

--- Python/Day1.py
+++ Python/Day1.py
@@ -1,10 +1,12 @@
 from GetProblem import get_problem
+from collections import Counter
 
-if __name__ == "__main__":
+
+def part_1() -> None:
     foo = get_problem(1, 2024)
-
     # split the data number (string)
     left_and_rights = [line.split() for line in foo.split("\n") if len(line)]
+
     # Transpose the list
     left, right = map(list, zip(*left_and_rights))
 
@@ -12,4 +14,22 @@ if __name__ == "__main__":
     right = sorted([int(i) for i in right])
 
     output = sum(abs(l - r) for l, r in zip(left, right))
-    print(output)
+    return output
+
+
+def part_2() -> None:
+    foo = get_problem(1, 2024)
+    # split the data number (string)
+    left_and_rights = [line.split() for line in foo.split("\n") if len(line)]
+    # Transpose the list
+    left, right = map(list, zip(*left_and_rights))
+
+    left = sorted([int(i) for i in left])
+    right = Counter([int(i) for i in right])
+
+    output = sum(l * right[l] for l in left)
+    return output
+
+
+if __name__ == "__main__":
+    print(part_2())
--- Python/GetProblem.py
+++ Python/GetProblem.py
@@ -2,18 +2,23 @@ import urllib.request
 
 
 def get_problem(day: int, year: int):
-    save_path = "../ProblemInputs/foo.txt"
-    with open(save_path, "r") as f:
-        content = f.read()
-    return content
-    cookie_value = ""
-    request = urllib.request.Request("https://adventofcode.com/2024/day/1/input")
-    request.add_header("Cookie", f"session={cookie_value}")
-
-    with urllib.request.urlopen(request) as r:
-        with open(save_path, "wb") as f:
-            content = r.read()
-            f.write(content)
+    save_path = f"../ProblemInputs/{year}-{day}.txt"
+    try:
+        with open(save_path, "r") as f:
+            content = f.read()
+    except FileNotFoundError:
+        cookie_value = ""
+        request = urllib.request.Request(
+            f"https://adventofcode.com/{year}/day/{day}/input"
+        )
+        request.add_header("Cookie", f"session={cookie_value}")
+        with urllib.request.urlopen(request) as r:
+            with open(save_path, "wb") as f:
+                content = r.read()
+                f.write(content)
+        content = content.decode()
+    finally:
+        return content
 
 
 if __name__ == "__main__":
