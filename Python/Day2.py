f27064121f514542adcf8c1f56605739c1b9ddcb
NitroSniper <mahiemiah35@gmail.com>
2024-12-03 14:25:24 +0000

    Problem 2 part 1 done)

--- Python/Day2.py
+++ Python/Day2.py
@@ -3,7 +3,24 @@ from GetProblem import get_problem
 
 @get_problem(2, 2024)
 def part_1(input_data) -> None:
-    pass
+    def per_case(line):
+        numbers = [int(number_str) for number_str in line.split()]
+        differences = [i - j for i, j in zip(numbers[:-1], numbers[1:])]
+        # Either True, None, False (True for ascending, False for descending)
+        order = set(x > 0 if x else None for x in differences)
+
+        # This filter out if 0 in the line and if it's not in order
+        if len(order) > 1:
+            return False
+
+        (is_ascending,) = order  # get the single element
+
+        # Order it depending if it is ascending and descending so the highest magnitude number is at the front
+        differences.sort(reverse=is_ascending)
+
+        return abs(differences[0]) <= 3
+
+    return sum(1 for line in input_data.split("\n") if line and per_case(line))
 
 
 @get_problem(2, 2024)
